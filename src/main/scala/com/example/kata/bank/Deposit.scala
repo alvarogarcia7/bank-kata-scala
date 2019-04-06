2f9a4ac52ae0f037b32fb4d1a7bfc34d5ba2d6e2
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-04-06 11:45:36 +0200

    Green

--- src/main/scala/com/example/kata/bank/Deposit.scala
+++ src/main/scala/com/example/kata/bank/Deposit.scala
@@ -4,6 +4,7 @@ package com.example.kata.bank;
 case class SuccessMessage(message: String)
 case class WelcomeMessage(message: String)
 case class PinRequired()
+case class NotLoggedIn()
 
 case class Deposit(amount: Int)
 case class InsertCard(cardNumber: String)
