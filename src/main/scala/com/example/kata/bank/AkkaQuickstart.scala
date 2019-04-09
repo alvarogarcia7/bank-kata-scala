b8c96232558ab816d5d8e04a6bb30a45ca552312
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-04-09 23:19:42 +0200

    Add a main to experience first hand the desired behaviour

--- src/main/scala/com/example/kata/bank/AkkaQuickstart.scala
+++ src/main/scala/com/example/kata/bank/AkkaQuickstart.scala
@@ -4,8 +4,19 @@ import akka.actor.ActorSystem
 
 object AkkaQuickstart extends App {
 
-  // Create the 'helloAkka' actor system
   val system: ActorSystem = ActorSystem("helloAkka")
 
   // Execute the Main here
+
+  val printer = new ScreenPrinter()
+  val userIdentification = system.actorOf(PrinterActor.props(printer), "userIdentification")
+  val printerActor = system.actorOf(PrinterActor.props(printer), "printer")
+  val atm = system.actorOf(ATM.props(userIdentification, printerActor), "ATM")
+
+
+  atm ! InsertCard("4000-0000-0000-000")
+  atm ! TypePin("0000")
+  atm ! TypePin("0000")
+  atm ! TypePin("0000")
+  atm ! TypePin("0123")
 }
